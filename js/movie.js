c379f1e712fae7dd2b7ac2c7d09a674c000f78ac
Henry Chon <47991972+chonhenry@users.noreply.github.com>
2020-03-22 22:11:49 -0400

    added movie details section

--- js/movie.js
+++ js/movie.js
@@ -1,13 +1,30 @@
 // console.log(window.temp);
 
-const params = new URLSearchParams(window.location.search);
+// const params = new URLSearchParams(window.location.search);
 
-console.log(`Title: ${params.get("title")}`);
+// console.log(`Title: ${params.get("title")}`);
 
-// fetch(`http://www.omdbapi.com/?apikey=cbfe887d&t=${params.get("title")}!`)
+// fetch(`http://www.omdbapi.com/?apikey=cbfe887d&t=Captain marvel`)
 //   .then(response => {
 //     return response.json();
 //   })
 //   .then(data => {
 //     console.log(data);
 //   });
+
+// Title: "Wonder Woman 1984";
+// Rated: "N/A";
+// Released: "05 Jun 2020";
+// Runtime: "N/A";
+// Genre: "Action, Adventure, Fantasy";
+// Director: "Patty Jenkins";
+// Actors: "Pedro Pascal, Gal Gadot, Kristen Wiig, Chris Pine";
+// Plot: "Plot unknown. A sequel to the 2017 superhero film 'Wonder Woman.'";
+// Language: "English";
+// Country: "USA, UK, Canada, Mexico, Spain";
+// Poster: "https://m.media-amazon.com/images/M/MV5BYzAyOGJkMzUtMmRjYS00NGJmLWExNGEtYzI2YjVmMmQzMzFiXkEyXkFqcGdeQXVyMTkxNjUyNQ@@._V1_SX300.jpg";
+// imdbID: "tt7126948";
+// Type: "movie";
+// DVD: "N/A";
+// Production: "Warner Bros. Pictures";
+// Rated:
